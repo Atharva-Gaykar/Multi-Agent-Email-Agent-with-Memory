7833f172c314bc1b9951a3ddfe77a35ed3787110
Atharva-Gaykar <gaykaratharva7@gmail.com>
2026-06-13 12:24:48 +0530

    removed debug

--- app/graph.py
+++ app/graph.py
@@ -133,6 +133,6 @@ builder.add_edge("store_memory_and_data_node", END)
 builder.add_edge("unsafe_emails_node", END)
 builder.add_edge("archive_node", END)
 
-graph=builder.compile(checkpointer=checkpointer, store=memory_store,debug=True)
+graph=builder.compile(checkpointer=checkpointer, store=memory_store)
 
 
